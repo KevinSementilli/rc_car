4e68b2aab611fceb2ad1c2e741dada0476f79fa5
KevinSementilli <ksementilli@hotmail.ca>
2026-08-03 19:39:25 -0400

    teleop config and launch

---
+++ redcat/launch/joystick.launch.py
@@ -0,0 +1,35 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    joy_params = os.path.join(
+        get_package_share_directory('redcat'),
+        'config',
+        'joystick.yaml'
+    )
+
+    joy_node = Node(
+        package='joy',
+        executable='joy_node',
+        name='joy_node',
+        output='screen',
+        parameters=[joy_params]
+    )
+
+    teleop_node = Node(
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        name='teleop_node',
+        output='screen',
+        parameters=[joy_params],
+        remappings=[('/cmd_vel', '/redcat/cmd_vel_unstamped')]
+    )
+
+    return LaunchDescription([
+        joy_node,
+        teleop_node
+    ])
